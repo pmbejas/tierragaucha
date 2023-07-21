0af4a7343a7bcb4a9d2113109973f84314e9ed24
Benjamin <benjapaz10@gmail.com>
2023-07-21 20:28:19 -0300

    primer commit

---
+++ clase 8/7/index.js
@@ -0,0 +1,38 @@
+console.log("hola mundo");
+
+Let InputNumber1 = 10;
+Let InputNumberTwo = 10;
+
+
+numero1 = "hola mundo";
+
+const COMPONENT_NAME = "Mi componente"
+
+
+var variable = 2; 
+
+
+
+
+//PROMPT
+Let inputData = prompt("ingrese un numero");
+console.log(inputData);
+
+alert(inputData);
+
+
+
+//Operdaores
+/*
+Esto es un codigo comentado
+*/
+
+//Let numberOne = prompt("ingrese un numero")
+//Let numberTwo = prompt("ingrese otro numero")
+//Let numerOne = 10 
+//Let numerTwo = 20;
+Let numberOne = prompt("ingrese un numero")
+Let numberTwo = prompt("ingrese otro numero")
+
+let noDefinido;
+
